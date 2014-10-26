06be556cd10383afb3a014db6765e529ea5b3e2a
zoejonestx91 <zoejonestx91@gmail.com>
2014-10-25 21:32:17 -0500

    Entity

---
+++ CyberPunkd/CyberPunkd/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CyberPunkd
+{
+    abstract class Entity : Drawable
+    {
+        protected Entity(Texture2D texture) : base(texture)
+        {
+        }
+    }
+}
