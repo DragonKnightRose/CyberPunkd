f7ecdd4d281fcd3e721bd3d43c697b225b13cc2d
zoejonestx91 <zoejonestx91@gmail.com>
2014-10-26 15:38:11 -0500

    Player and entity changes

---
+++ CyberPunkd/CyberPunkd/Entity.cs
@@ -0,0 +1,66 @@
+<<<<<<< Updated upstream
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CyberPunkd
+{
+    abstract class Entity : Drawable
+    {
+        protected Entity(Texture2D texture) : base(texture)
+        {
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CyberPunkd
+{
+    abstract class Entity : Drawable
+    {
+        //autoAnimate controls whether under the default draw(), we automatically cycle through
+        //the animations on a row. If you set this to true, make sure that animationSequenceLength[]
+        //is set.
+        //TODO: Implement this.
+        protected Boolean autoAnimate = false;
+        //animationSequenceLength is used to to represent how many animation frames are in each row.
+        protected int[] animationSequenceLength;
+
+        protected Entity(Texture2D texture) : base(texture)
+        {
+        }
+
+        public override void draw(GameTime gameTime, Point position)
+        {
+            spriteBatch.Draw(texture, new Vector2(position.X, position.Y), 
+                sourceRectangle: getFrameRectangle(currentFrame), color: Color.White);
+        }
+
+        public void update(GameTime gameTime)
+        {
+            
+        }
+
+        public void onCollision(Drawable drawable)
+        {
+            
+        }
+
+        // TODO: Question: Should the player be able to interact?
+        public void onTouch()
+        {
+            
+        }
+
+        
+    }
+}
+>>>>>>> Stashed changes
---
+++ CyberPunkd/CyberPunkd/Player.cs
@@ -0,0 +1,115 @@
+<<<<<<< Updated upstream
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CyberPunkd
+{
+
+    // TODO: Refractor frame selection code into Entity class.
+    class Player : Actor
+    {
+        private int gender = 0;
+
+        private Point sheetSize;
+        public Player(ContentManager content) : base(content)
+        {
+            texture = content.Load<Texture2D> (@"SpriteSheets\Female_sheet");
+            
+            sheetSize = new Point(texture.Width/64, texture.Height/64);
+        }
+
+       
+        
+       public override void draw(GameTime gameTime,  Point position)
+        {
+            spriteBatch.Draw(texture,Vector2.Zero,getFrameRectangle(currentFrame),Color.White);
+            //throw new NotImplementedException();
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CyberPunkd
+{
+
+    // TODO: Refractor frame selection code into Entity class.
+    class Player : DynamicEntity
+    {
+        private int gender = 0;
+        private int directionOffset = 0;
+        private int modeOffset = 2;
+        private int spriteRow = 0;
+
+        private int SPELL_CAST = 0,
+                    THRUST = 1,
+                    WALK = 2,
+                    SLASH =3,
+                    SHOOT = 4,
+                    HURT = 5,
+                    SIT = 6;
+        
+
+        private Point sheetSize;
+        public Player(Texture2D texture) : base(texture)
+        {
+            //texture = content.Load<Texture2D> (@"SpriteSheets\Female_sheet");
+            
+            sheetSize = new Point(texture.Width/64, texture.Height/64);
+        }
+
+       
+        
+       public override void draw(GameTime gameTime,  Point position)
+        {
+            spriteBatch.Draw(texture,Vector2.Zero,getFrameRectangle(currentFrame),Color.White);
+            //throw new NotImplementedException();
+        }
+
+        public override void update(GameTime gameTime)
+        {
+            int oldSpriteRow = spriteRow;
+            KeyboardState kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Up))
+            {
+                directionOffset = 0;
+            }
+            else if (kb.IsKeyDown(Keys.A) || kb.IsKeyDown(Keys.Left))
+            {
+                directionOffset = 1;
+            }
+            else if (kb.IsKeyDown(Keys.S) || kb.IsKeyDown(Keys.Down))
+            {
+                directionOffset = 2;
+            }
+            else if (kb.IsKeyDown(Keys.D) || kb.IsKeyDown(Keys.Right))
+            {
+                directionOffset = 3;
+            }
+            if (modeOffset != HURT)
+                spriteRow = modeOffset*4 + directionOffset;
+            else
+                spriteRow = modeOffset*4;
+
+            if(oldSpriteRow != spriteRow)
+                setSpriteFrame(new Point(spriteRow,0));
+
+
+        }
+    }
+}
+>>>>>>> Stashed changes
